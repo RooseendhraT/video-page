b0329b82149ab8c9b19fb2f9cf4a66da03b533d3
RooseendhraT <rooseendhrat@gmail.com>
2025-01-23 13:19:10 +0530

    fixed homepage path and router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/Home.css';
-import './styles/Navbar.css';
-import './styles/Modal.css';
-import './styles/videoCard.css' // Your custom styles (optional)
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap'; // Import Bootstrap JS (bundle includes all necessary JS)
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+// Styles
+import './styles/Home.css';
+import './styles/Navbar.css';
+import './styles/Modal.css';
+import './styles/videoCard.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
-  <Router>
+  <Router basename="/video-page"> {/* Add basename here */}
     <App />
   </Router>
 );
